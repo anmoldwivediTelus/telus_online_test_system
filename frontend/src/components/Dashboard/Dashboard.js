ed50b98326ad91f63259e7d3785f33ce0223a7de
pratyushgoyal08 <pratyush.goyal@telusinternationl.com>
2024-11-14 13:31:56 +0530

    responsive design, navbar collasable.

---
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+// Dashboard.js
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div className="dashboard-content">
+      <p>No content to show as of now</p>
+    </div>
+  );
+}
+
+export default Dashboard;
