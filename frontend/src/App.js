8454af986411a0057a133463c8825562adcf315e
anjali verma <anjali.verma@telusinternational.com>
2024-11-22 16:29:30 +0530

    test page updated without header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,37 +14,60 @@ import UserList from './components/Admin/UserList'
 import AdminQuestion from './components/Admin/AdminQuestion'
 import RecruiterHomepage from './components/Admin/RecruiterHomepage'
 // import UserList from './components/Admin/UserList'
-const App = () => {
+
+
+ // Layout with Header
+const LayoutWithHeader = () => (
+  <div>
+    <Header />
+    <Outlet /> {/* Renders the current route */}
+  </div>
+);
+
+// Layout without Header
+const LayoutWithoutHeader = () => (
+  <div>
+    <Outlet /> {/* Renders the current route */}
+  </div>
+);
+
   const appRouter  = createBrowserRouter([
     {
-      path:'/',
-      element:<OnlineTestInstructions/>
+      element: <LayoutWithHeader />, // Default layout with Header
+      children: [
+        {
+          path: '/',
+          element: <OnlineTestInstructions />,
+        },
+        {
+          path: '/admin',
+          element: <RecruiterHomepage />,
+        },
+        {
+          path: '/exit',
+          element: <ExitPage />,
+        },
+        {
+          path: '/question',
+          element: <AdminQuestion />,
+        },
+      ],
     },
-    {
-      path:'/admin',
-      element:<RecruiterHomepage/>,
-    },
-    {
-      path:'/exit',
-      element:<ExitPage/>,
-    },
-    {
-      path:'/test',
-      element:<Test/>,
-    },
-    {
-      path:'/question',
-      element:<AdminQuestion/>,
-    }
-  ])
-  return (
-    <div>
-      <Header/>
-      <RouterProvider router={appRouter}/>
-      {/* <div>footer</div> */}
-    </div>
-     
-  )
-}
+
+    // without Header
+   {
+    element: <LayoutWithoutHeader />, 
+    children: [
+      {
+        path: '/test',
+        element: <Test />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={appRouter} />;
+};
 
 export default App;
